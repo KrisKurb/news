d1a4fabaa4777e7c84b632bb76eb3b403cc76102
KrisKurb <kris_ra@mail.ru>
2020-05-22 23:11:45 +0300

    news

---
+++ src/Controller/Main/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
